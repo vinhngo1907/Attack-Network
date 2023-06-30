b5115062a7e3498ff100fc11cfbe850bdc254941
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-30 19:42:58 +0700

    commit

--- Server.py
+++ Server.py
@@ -1,29 +1,31 @@
 import sys, socket
 from os import listdir
 from os.path import isfile, join
+
 from ServerWorker import ServerWorker
 
-class Server:
-    def main(self):
-        try:
-            SERVER_PORT = int(sys.argv[1])
-        except:
-            print("[Usage: Server.py Server_port]\n")
-        rtspSocket = socket.socket(socket.AF_INET, socket.SOCKET_STREAM)
+class Server:	
+	
+	def main(self):
+		try:
+			SERVER_PORT = int(sys.argv[1])
+		except:
+			print("[Usage: Server.py Server_port]\n")
+		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        rtspSocket.bind(('', SERVER_PORT))
-        rtspSocket.listen(5)
-        fileSocket = socket.socket(socket.AF_INET, socket.SOCKET_STREAM)
-        fileSocket.bind(('',12345))
-        fileSocket.listen(1)
-        n = 0
-        # Receive client info (address, port) through RTSP/TCP session
+		rtspSocket.bind(('', SERVER_PORT))
+		rtspSocket.listen(5)   
+		fileSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		fileSocket.bind(('', 12345))
+		fileSocket.listen(1)
+		n = 0
+		# Receive client info (address,port) through RTSP/TCP session
+		while True:
 
-        while true:
-            clientInfo = {}
-            clientInfo['rtspSocket'] = rtspSocket.accept()
-            clientInfo['fileSocket'] = fileSocket.accept()
-            ServerWorker(clientInfo).run()
+			clientInfo = {}
+			clientInfo['rtspSocket'] = rtspSocket.accept()
+			clientInfo['fileSocket'] = fileSocket.accept()
+			ServerWorker(clientInfo).run()		
 
 if __name__ == "__main__":
-    (Server()).main()
+	(Server()).main()
