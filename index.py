e3534c951de4d215cb81b67188230c0222823f53
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-04-28 12:08:45 +0700

    commit

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 from requests_html import HTMLSession
-import csv, numpy as np, pandas as pd
+import csv
+import numpy as np
+import pandas as pd
 
 session = HTMLSession()
-r = session.get("https://tiki.vn")
+r = session.get("https://tiki.vn/")
 r.encoding = "utf-8"
 
 data = []
@@ -10,10 +12,17 @@ link_category_dict = dict()
 
 
 def parse_link(href, currentPage=1):
-    page = href + "&page=" + str(currentPage)
+    # Check if href already starts with 'https://'
+    if href.startswith("https://"):
+        page = href + f"?page={currentPage}"
+    else:
+        base_url = "https://tiki.vn"
+        page = f"{base_url}{href}?page={currentPage}"
+
+    # print("page", page)
     _session = HTMLSession()
-    _r = _session(page)
-    items = _r.html.find(".content .title")
+    _r = _session.get(page)
+    items = _r.html.find("div.ProductList__NewWrapper-sc-1dl80l2-0.jXFjHV")
 
     if len(items) == 0:
         return
@@ -24,39 +33,49 @@ def parse_link(href, currentPage=1):
 
     try:
         print("page ", page)
-        parse_link(href, nextPage)
+        parse_link(href, currentPage=nextPage)
     except Exception as e:
-        print("some errors occured", str(e))
+        print("some errors occured when parsing link", str(e))
 
 
 def get_categories():
-    categories = []
-    item_list = r.html.find("li.MenuItem-tii3xq-0")
-    for item in item_list:
-        href = item.find("a", first=True).attrs["href"]
-        category = item.text
-        categories.append((href, category))
-        link_category_dict[href] = category
-    return categories
+    try:
+        categories = []
+        item_list = r.html.find("div.styles__StyledItemV2-sc-oho8ay-1.bHIPhv")
+        for item in item_list:
+            href = item.find("a", first=True).attrs["href"]
+            category = item.text
+            categories.append((href, category))
+            link_category_dict[href] = category
+        return categories
+    except Exception as e:
+        print("some errors occured when getting categories", str(e))
 
 
 def crawl_data():
-    categories_tuples = get_categories()
-    for category_tuple in categories_tuples:
-        (href, category_name) = category_tuple
-        parse_link(href)
+    try:
+        category_tuples = get_categories()
+        for category_tuple in category_tuples:
+            (href, category_name) = category_tuple
+            parse_link(href)
+    except Exception as e:
+        print("some errors occured when crawling data", str(e))
 
 
 def save_data():
-    np.random.shuffle(data)
-    if len(data) != 0:
-        df = pd.DataFrame(data, columns=["product_title", "category"])
-        df.to_csv("product.csv", index=False, encoding="utf-8")
-        # with open('product.csv', 'w', newline='', encoding='utf-8') as file:
-        # writer = csv.writer(file)
-        # writer.writerow(["product_title", "category"])
-        # for item in data:
-        # writer.writerow(item)
+    print("??????", data)
+    try:
+        np.random.shuffle(data)
+        if len(data) != 0:
+            df = pd.DataFrame(data, columns=["product_title", "category"])
+            df.to_csv("product.csv", index=False, encoding="utf-8")
+            # with open('product.csv', 'w', newline='', encoding='utf-8') as file:
+            # writer = csv.writer(file)
+            # writer.writerow(["product_title", "category"])
+            # for item in data:
+            # writer.writerow(item)
+    except Exception as e:
+        print("some errors occured when saving data", str(e))
 
 
 category_tutples = get_categories()
@@ -66,5 +85,6 @@ def run():
     print("Start crawling")
     crawl_data()
     save_data()
+
 
 run()
